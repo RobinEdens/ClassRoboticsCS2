c9a7eff7727299790c4b5c6b1e1f5f0f0294ce90
RobinHase <Jonahanderson42@gmail.com>
2017-03-01 17:29:10 -0600

    Test Tomorrow (should work??)

--- src/ShortestPath.java
+++ src/ShortestPath.java
@@ -12,21 +12,25 @@ import edu.bismarckstate.shortestpathhelper.util.MovementInstructions;
 
 public class ShortestPath {
 
-	
 	public static void main (String[] args) {
 		MovementInstructions inst = new MovementInstructions();
 		MenuController testMenu = new MenuController();
 		testMenu.setInstructions(inst);
 		ArrayList<MovementInstruction> tstList = new ArrayList<MovementInstruction> (testMenu.Start());
-		Double [][] test = new Double[tstList.size()][2];
-		int i = 0;
+		Double [][] vectors = new Double[tstList.size()][2];
+		int finalVector, finalAngle, combX, combY, i = 0;	
 		for (MovementInstruction mi : tstList)
 		{
-			test[i][0] = mi.getDistance()* Math.cos((mi.getDirection()*Math.PI)/180);
-			test[i][1] = mi.getDistance()* Math.sin((mi.getDirection()*Math.PI)/180);
-			System.out.println("(" + test[i][0] + ", " + test[i][1] + ")");
+			vectors[i][0] = mi.getDistance()* Math.cos((mi.getDirection()*Math.PI)/180);
+			combX += vectors[i][0];
+			vectors[i][1] = mi.getDistance()* Math.sin((mi.getDirection()*Math.PI)/180);
+			combY += vectors[i][1];
+			System.out.println("(" + vectors[i][0] + ", " + vectors[i][1] + ")");
 			i++;
 		}
+		finalVector = Math.hypot(combX, combY);
+		finalAngle = Math.atan(combY/combX);
+		System.out.println("\nOptimized Instructions: Angle " + finalAngle + ", Distance " + finalVector);
 		Button.ENTER.waitForPressAndRelease();
 	}
 }
