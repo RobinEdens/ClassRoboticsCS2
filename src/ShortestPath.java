1d7493c9937c35f15014f3535c4a3cd0553d58ec
Anderson <jonah.anderson@ndus.edu>
2017-03-02 10:46:24 -0600

    mostly final

--- src/ShortestPath.java
+++ src/ShortestPath.java
@@ -14,11 +14,13 @@ public class ShortestPath {
 
 	public static void main (String[] args) {
 		MovementInstructions inst = new MovementInstructions();
-		MenuController testMenu = new MenuController();
-		testMenu.setInstructions(inst);
-		ArrayList<MovementInstruction> tstList = new ArrayList<MovementInstruction> (testMenu.Start());
+		MenuController menu = new MenuController();
+		menu.setInstructions(inst);
+		ArrayList<MovementInstruction> tstList = new ArrayList<MovementInstruction> (menu.Start());
 		Double [][] vectors = new Double[tstList.size()][2];
-		int finalVector, finalAngle, combX, combY, i = 0;	
+		double finalDistance, finalAngle, combY = 0;	
+		double combX = 0;
+		int i = 0;
 		for (MovementInstruction mi : tstList)
 		{
 			vectors[i][0] = mi.getDistance()* Math.cos((mi.getDirection()*Math.PI)/180);
@@ -28,9 +30,21 @@ public class ShortestPath {
 			System.out.println("(" + vectors[i][0] + ", " + vectors[i][1] + ")");
 			i++;
 		}
-		finalVector = Math.hypot(combX, combY);
-		finalAngle = Math.atan(combY/combX);
-		System.out.println("\nOptimized Instructions: Angle " + finalAngle + ", Distance " + finalVector);
+		finalDistance = Math.sqrt(Math.pow(combX, 2) + Math.pow(combY, 2));
+		finalAngle = (Math.atan2(combY, combX) * 180) / Math.PI;
+		MovementInstruction finalMove = new MovementInstruction(finalAngle, finalDistance);
 		Button.ENTER.waitForPressAndRelease();
+		
+		RobotMovement rob = new RobotMovement();
+		if (rob.isDiffPilotOk()){
+			System.out.println("Woohoo it worked");
+			Button.ENTER.waitForPressAndRelease();
+		}else{
+			System.out.println("There was an error");
+			Button.ENTER.waitForPressAndRelease();
+		}
+		MovementInstruction wheel = new MovementInstruction(90,0);
+		rob.move(wheel);
+		
 	}
 }
